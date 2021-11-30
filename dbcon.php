bcefc0e6c84cc294b2de5fc2ed5ed95de29a33ad
Imamul Kadir <imamulkadiroyan@gmail.com>
2021-11-30 20:24:25 +0600

    remote_mysql

--- dbcon.php
+++ dbcon.php
@@ -1,6 +1,7 @@
 <?php
 
-  $conn = new mysqli("localhost","root","","sms");
+// $conn = new mysqli("localhost","root","","sms");
+  $conn = new mysqli("remotemysql.com","UcsTG9fnoW","y1YngqHZrx","UcsTG9fnoW");
 
 
  ?>
